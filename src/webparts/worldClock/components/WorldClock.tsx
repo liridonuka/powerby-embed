c2dc35aec03ff627f2de78826034293218182de7
Liridon Uka <liridon@liridon365.onmicrosoft.com>
2020-11-05 19:50:38 +0100

    Time clocks table css

--- src/webparts/worldClock/components/WorldClock.tsx
+++ src/webparts/worldClock/components/WorldClock.tsx
@@ -14,9 +14,9 @@ export default class WorldClock extends React.Component<IWorldClockProps, {}> {
   public render(): React.ReactElement<IWorldClockProps> {
     return (
       <div className={styles.worldTime}>
-        <div className={styles.divHeader}>Clocks</div>
+        {/* <div className={styles.divHeader}>Clocks</div> */}
         {/* PLace/City Rows starts here */}
-        <div className={styles.divFull}>
+        {/* <div className={styles.divFull}>
           <div className={styles.firstColumn}>
             <img
               width="50px"
@@ -27,99 +27,91 @@ export default class WorldClock extends React.Component<IWorldClockProps, {}> {
           </div>
           <div className={styles.secondColumn}>
             <div className={styles.description}>
-              {/* {this.props.description
+              {this.props.description
                 ? this.props.description
-                : strings.LocalTimeDescription} */}
+                : strings.LocalTimeDescription}
               London
             </div>
           </div>
           <div className={styles.thirdColumn}>
             <Clock
               timeZoneOffset={this.convertTimeZoneIdToOffset(
-                // this.props.timeZoneOffset
+                this.props.timeZoneOffset
                 48
               )}
             />
           </div>
-        </div>
-        <div className={styles.divFull}>
-          <div className={styles.firstColumn}>
-            <img
-              width="50px"
-              height="38px"
-              src="https://aralytiks.sharepoint.com/sites/Dupont/SiteAssets/DuPont%20Images/Flags/chinaFlag.png"
-              alt=""
-            />
-          </div>
-          <div className={styles.secondColumn}>
-            <div className={styles.description}>
-              {/* {this.props.description
-                ? this.props.description
-                : strings.LocalTimeDescription} */}
-              Shanghai
-            </div>
-          </div>
-          <div className={styles.thirdColumn}>
-            <Clock
-              timeZoneOffset={this.convertTimeZoneIdToOffset(
-                // this.props.timeZoneOffset
-                136
-              )}
-            />
-          </div>
-        </div>
-        <div className={styles.divFull}>
-          <div className={styles.firstColumn}>
-            <img
-              width="50px"
-              height="38px"
-              src="https://aralytiks.sharepoint.com/sites/Dupont/SiteAssets/DuPont%20Images/Flags/netherlandsFlag.png"
-              alt=""
-            />
-          </div>
-          <div className={styles.secondColumn}>
-            <div className={styles.description}>
-              {/* {this.props.description
-                ? this.props.description
-                : strings.LocalTimeDescription} */}
-              Zanndam
-            </div>
-          </div>
-          <div className={styles.thirdColumn}>
-            <Clock
-              timeZoneOffset={this.convertTimeZoneIdToOffset(
-                // this.props.timeZoneOffset
-                50
-              )}
-            />
-          </div>
-        </div>
-        <div className={styles.divFull}>
-          <div className={styles.firstColumn}>
-            <img
-              width="50px"
-              height="38px"
-              src="https://aralytiks.sharepoint.com/sites/Dupont/SiteAssets/DuPont%20Images/Flags/usaFlag.png"
-              alt=""
-            />
-          </div>
-          <div className={styles.secondColumn}>
-            <div className={styles.description}>
-              {/* {this.props.description
-                ? this.props.description
-                : strings.LocalTimeDescription} */}
-              Wilmington
-            </div>
-          </div>
-          <div className={styles.thirdColumn}>
-            <Clock
-              timeZoneOffset={this.convertTimeZoneIdToOffset(
-                // this.props.timeZoneOffset
-                21
-              )}
-            />
-          </div>
-        </div>
+        </div> */}
+        <table className={styles.customers}>
+          <tr>
+            <th style={{ fontSize: "15px" }}>Clocks</th>
+            <th></th>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.contentos}>
+                <img
+                  width="60px"
+                  height="45px"
+                  src="https://aralytiks.sharepoint.com/sites/Dupont/SiteAssets/DuPont%20Images/Flags/ukFlag.png"
+                  alt=""
+                />
+                <p>London</p>
+              </div>
+            </td>
+            <td>
+              <Clock timeZoneOffset={this.convertTimeZoneIdToOffset(48)} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.contentos}>
+                <img
+                  width="60px"
+                  height="45px"
+                  src="https://aralytiks.sharepoint.com/sites/Dupont/SiteAssets/DuPont%20Images/Flags/chinaFlag.png"
+                  alt=""
+                />
+                <p>Shanghai</p>
+              </div>
+            </td>
+            <td>
+              <Clock timeZoneOffset={this.convertTimeZoneIdToOffset(136)} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.contentos}>
+                <img
+                  width="60px"
+                  height="45px"
+                  src="https://aralytiks.sharepoint.com/sites/Dupont/SiteAssets/DuPont%20Images/Flags/netherlandsFlag.png"
+                  alt=""
+                />
+                <p>Zanndam</p>
+              </div>
+            </td>
+            <td>
+              <Clock timeZoneOffset={this.convertTimeZoneIdToOffset(50)} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.contentos}>
+                <img
+                  width="60px"
+                  height="45px"
+                  src="https://aralytiks.sharepoint.com/sites/Dupont/SiteAssets/DuPont%20Images/Flags/usaFlag.png"
+                  alt=""
+                />
+                <p>Wilmington</p>
+              </div>
+            </td>
+            <td>
+              <Clock timeZoneOffset={this.convertTimeZoneIdToOffset(21)} />
+            </td>
+          </tr>
+        </table>
       </div>
     );
   }
